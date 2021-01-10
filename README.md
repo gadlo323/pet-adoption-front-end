4203f5f9a59e3bad228dc29fd35236cc19d86386
isayas <gadlo323@gmail.com>
2021-01-10 11:01:56 +0200

    fix:deploy issue > Done!

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# pet progect url
+
+https://adoptefriend.netlify.app
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/conteaxts/AutoConteaxt.js
+++ src/conteaxts/AutoConteaxt.js
@@ -14,29 +14,29 @@ export const AutoProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const history = useHistory();
   const baseUrl = "https://adoptfriend.herokuapp.com";
-  const objCookies = {
-    withCredentials: true,
-  };
 
   //userActive
   const userActive = async () => {
     try {
-      const isToken = Cookies.get("token");
-      if (isToken) {
-        const tokenRes = await axios.post(
-          `${baseUrl}/user/tokenValid`,
-          {},
-          objCookies
-        );
-        const user = jwt(tokenRes.data);
-        setCurrentUser(user);
-        setLoading(false);
+      const token = JSON.parse(localStorage.getItem("token"));
+      if (token) {
+        const tokenRes = await axios.post(`${baseUrl}/user/tokenValid`, null, {
+          headers: {
+            authorization: "Bearer " + token,
+          },
+        });
+
+        if (tokenRes.data) {
+          const user = jwt(token);
+          setCurrentUser(user);
+          setLoading(false);
+        }
       } else {
         setLoading(false);
       }
     } catch (err) {
       setLoading(false);
-      return err.response.data;
+      throw err;
     }
   };
 
@@ -48,20 +48,30 @@ export const AutoProvider = ({ children }) => {
   //signUp
   const signupUser = async (data) => {
     try {
-      const res = await axios.post(`${baseUrl}/signup`, data, objCookies);
-      setCurrentUser(res.data);
-      return { error: 0, dataSevere: res.data };
+      const res = await axios.post(`${baseUrl}/signup`, data);
+      localStorage.setItem("token", JSON.stringify(res.data));
+      const user = jwt(res.data);
+      setCurrentUser(user);
+      return { error: 0, dataSevere: user };
     } catch (error) {
       return { error: 1, dataSevere: error.response.data };
     }
   };
   //login
-  const hendaleLogin = async (data) => {
+  const hendaleLogin = async (formInfo) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    let config = {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    };
     try {
-      const res = await axios.post(`${baseUrl}/login`, data, objCookies);
+      const res = await axios.post(`${baseUrl}/login`, formInfo, config);
       if (res.data) {
-        setCurrentUser(res.data);
-        return res.data;
+        localStorage.setItem("token", JSON.stringify(res.data));
+        const user = jwt(res.data);
+        setCurrentUser(user);
+        return user;
       }
     } catch (error) {
       return false;
@@ -71,18 +81,15 @@ export const AutoProvider = ({ children }) => {
   //Logout
 
   const logOut = () => {
-    Cookies.remove("token");
+    // Cookies.remove("token");
+    localStorage.removeItem("token");
     setCurrentUser();
     history.push("/");
   };
   //update
   const hendlaUpdate = async (data) => {
     try {
-      const res = await axios.patch(
-        `${baseUrl}/user/${currentUser.uId}`,
-        data,
-        objCookies
-      );
+      const res = await axios.patch(`${baseUrl}/user/${currentUser.uId}`, data);
       return res.data;
     } catch (err) {
       return err.response.data;
@@ -92,7 +99,7 @@ export const AutoProvider = ({ children }) => {
   //getUser full
   const getUser = async (uId) => {
     try {
-      const res = await axios.get(`${baseUrl}/userFull/${uId}`, objCookies);
+      const res = await axios.get(`${baseUrl}/userFull/${uId}`);
       const data = res.data;
       return data;
     } catch (err) {
@@ -102,7 +109,7 @@ export const AutoProvider = ({ children }) => {
   //getUser basic info
   const userInfo = async (uId) => {
     try {
-      const res = await axios.get(`${baseUrl}/user/${uId}`, objCookies);
+      const res = await axios.get(`${baseUrl}/user/${uId}`);
       const data = res.data;
       return data;
     } catch (err) {
@@ -112,8 +119,14 @@ export const AutoProvider = ({ children }) => {
 
   //addPet to db
   const addPet = async (content) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    let config = {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    };
     try {
-      const res = await axios.post(`${baseUrl}/addpet`, content, objCookies);
+      const res = await axios.post(`${baseUrl}/addpet`, content, config);
       if (res.data) {
         return res.data;
       }
@@ -124,11 +137,17 @@ export const AutoProvider = ({ children }) => {
 
   //edit Pet to db
   const editPet = async (content, id, cloudinary_id) => {
+    const token = JSON.parse(localStorage.getItem("token"));
+    let config = {
+      headers: {
+        authorization: "Bearer " + token,
+      },
+    };
     try {
       const res = await axios.patch(
         `${baseUrl}/editpet/?id=${id}&cloudId=${cloudinary_id}`,
         content,
-        objCookies
+        config
       );
       if (res.data) {
         return res.data;
@@ -141,7 +160,7 @@ export const AutoProvider = ({ children }) => {
   //get one Pet from db
   const getPet = async (id) => {
     try {
-      const res = await axios.get(`${baseUrl}/pet/${id}`, objCookies);
+      const res = await axios.get(`${baseUrl}/pet/${id}`);
       if (res.data) {
         return res.data;
       }
@@ -170,8 +189,7 @@ export const AutoProvider = ({ children }) => {
     try {
       const res = await axios.post(
         `${baseUrl}/adopteFoster/${currentUser.uId}`,
-        { data: data, type: type },
-        objCookies
+        { data: data, type: type }
       );
       return res.data;
     } catch (err) {
@@ -183,8 +201,7 @@ export const AutoProvider = ({ children }) => {
     try {
       const res = await axios.post(
         `${baseUrl}/savePet/${currentUser.uId}`,
-        data,
-        objCookies
+        data
       );
       return res.data;
     } catch (err) {
@@ -196,8 +213,7 @@ export const AutoProvider = ({ children }) => {
   const returnsavePet = async (id) => {
     try {
       const res = await axios.delete(
-        `${baseUrl}/deletesavepet/?uId=${currentUser.uId}&petid=${id}`,
-        objCookies
+        `${baseUrl}/deletesavepet/?uId=${currentUser.uId}&petid=${id}`
       );
       return res.data;
     } catch (err) {
@@ -208,10 +224,7 @@ export const AutoProvider = ({ children }) => {
   //get user owned pets/saved
   const getPets = async () => {
     try {
-      const res = await axios.get(
-        `${baseUrl}/myPets/${currentUser.uId}`,
-        objCookies
-      );
+      const res = await axios.get(`${baseUrl}/myPets/${currentUser.uId}`);
       return res.data;
     } catch (err) {
       return err.response.data;
@@ -221,9 +234,15 @@ export const AutoProvider = ({ children }) => {
   //get all users
   const getUsers = async (page, perPage) => {
     try {
+      const token = JSON.parse(localStorage.getItem("token"));
+      let config = {
+        headers: {
+          authorization: "Bearer " + token,
+        },
+      };
       const res = await axios.get(
         `${baseUrl}/getusers/?page=${page}&per_page=${perPage}`,
-        objCookies
+        config
       );
       return res.data;
     } catch (err) {
@@ -235,8 +254,7 @@ export const AutoProvider = ({ children }) => {
   const returnPet = async (id) => {
     try {
       const res = await axios.post(
-        `${baseUrl}/returnPet/?uId=${currentUser.uId}&petid=${id}`,
-        objCookies
+        `${baseUrl}/returnPet/?uId=${currentUser.uId}&petid=${id}`
       );
       return res.data;
     } catch (err) {
